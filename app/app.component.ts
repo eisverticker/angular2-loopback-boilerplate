22b2fce634650e74a0752dc99229b9215099d911
Alexander Noack <eisverticker@gmail.com>
2016-08-14 13:30:34 +0200

    updated ng2-translate to the module version and refactored language detection code

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 import { TranslateService } from 'ng2-translate/ng2-translate';
+import { I18nHelperService } from './i18n/i18n-helper.service';
 import { NotificationService } from './utilities/notification.service';
 import { Notification } from './utilities/notification';
 import { AuthService } from './loopback-auth/auth.service';
@@ -22,28 +23,10 @@ export class AppComponent implements OnInit{
     private translate: TranslateService,
     private notify: NotificationService,
     private toastr: ToastsManager,
-    private auth: AuthService
+    private auth: AuthService,
+    private i18n: I18nHelperService
   ){
-    let supportedLanguages: string[] = [
-      'de',
-      'en',
-      'ru'
-    ];
-    //- split the IETF language tag given by the user agent
-    //- we only need the first part (e.g. 'en' from 'en-US')
-    //  which probably a ISO-639-1 tag
-    //- so we can keep it simple
-    let browserLang: string = navigator.language.split('-')[0];
-
-    //make sure to add the language codes of all supported languages
-    let lang = supportedLanguages.indexOf(browserLang) === -1 ? 'en': browserLang;
-
-     //the language to use
-    this.translate.use(lang);
-
-    //this language will be used as a fallback if a translation wasn't found in the current language
-    this.translate.setDefaultLang('en');
-
+    this.i18n.detectUserLanguage();
   }
 
   ngOnInit(){
--- app/i18n/i18n.module.ts
+++ app/i18n/i18n.module.ts
@@ -1,31 +1,29 @@
 import { NgModule } from '@angular/core';
 import {
+  TranslateModule,
   TranslateService,
-  TranslatePipe,
   TranslateLoader,
   TranslateStaticLoader
 } from 'ng2-translate/ng2-translate';
+import { I18nHelperService } from './i18n-helper.service';
 
 import { Http } from '@angular/http';
 
 
 @NgModule({
   imports:      [
-
+    TranslateModule.forRoot({
+      provide: TranslateLoader,
+      useFactory: (http: Http) => new TranslateStaticLoader(http, '/web/locale', '.json'),
+      deps: [Http]
+    })
   ],
-  declarations: [
-    TranslatePipe
-  ],
+  declarations: [],
   exports:      [
-    TranslatePipe
+    TranslateModule
   ],
   providers: [
-    TranslateService,
-    {
-      provide: TranslateLoader,
-      useFactory: (http: Http) => new TranslateStaticLoader(http, 'web/locale', '.json'),
-      deps: [Http]
-    }
+    I18nHelperService
   ]
 })
 export class I18nModule {
